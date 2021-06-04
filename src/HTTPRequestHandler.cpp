bca791eae8c5172652bd3fda92b67f174678e4c0
Vitaliy <mr.prikota@gmail.com>
2021-06-04 15:10:28 +0300

    Switched from raw char pointers to STL containers

--- src/HTTPRequestHandler.cpp
+++ src/HTTPRequestHandler.cpp
@@ -8,7 +8,7 @@
 #include <regex>
 
 #define REQUEST_BUFFER_SIZE 4096
-#define RESPONSE_BUFFER_SIZE 8192
+#define RESPONSE_BUFFER_SIZE 4096
 #define FILE_BUFFER_SIZE 4096
 
 HTTPRequestHandler::HTTPRequestHandler() {
@@ -45,33 +45,43 @@ void HTTPRequestHandler::Handle(int socket) {
 }
 
 void HTTPRequestHandler::Respond(const HTTPRequest &request, int socket) {
-    char *rawResponse = new char[RESPONSE_BUFFER_SIZE];
-    char *fileBuffer = nullptr;
+    std::vector<char> rawHTTPResponseHeader;
+    std::vector<char> fileBuffer;
+    std::array<char, FILE_BUFFER_SIZE> readFileBuffer{};
     size_t fileReadAmount = 0;
     FILE *fileToSend;
     std::regex htmlRegex(".html|.htm", std::regex_constants::ECMAScript);
+    std::regex faviconRegex("favicon.ico", std::regex_constants::ECMAScript);
 
     fileToSend = fopen(("." + request.requestURI).c_str(), "rb");
+    rawHTTPResponseHeader.reserve(RESPONSE_BUFFER_SIZE);
     if (fileToSend == nullptr) {
         std::string errorMessage = "File " + request.requestURI + " wasn't found!\n";
-        snprintf(rawResponse, RESPONSE_BUFFER_SIZE,
+        snprintf(rawHTTPResponseHeader.data(), RESPONSE_BUFFER_SIZE,
                  "%s 404 NOTFOUND\nContent-Type: text/plain\nContent-Length: %lu\n\n%s",
                  request.httpVersion.c_str(), errorMessage.size(), errorMessage.c_str());
-        write(socket, rawResponse, strlen(rawResponse));
+        write(socket, rawHTTPResponseHeader.data(), strlen(rawHTTPResponseHeader.data()));
     } else {
-        fileBuffer = new char[FILE_BUFFER_SIZE];
-        fileReadAmount = fread(fileBuffer, sizeof(char), FILE_BUFFER_SIZE, fileToSend);
-        if (std::regex_search(request.requestURI, htmlRegex)) {
-            snprintf(rawResponse, RESPONSE_BUFFER_SIZE, "%s 200 OK\nContent-Type: text/html\nContent-Length: %lu\n"
-                                                        "\n%s", request.httpVersion.c_str(), fileReadAmount,
-                     fileBuffer);
-            write(socket, rawResponse, strlen(rawResponse));
+        while ((fileReadAmount = fread(readFileBuffer.data(), sizeof(char), FILE_BUFFER_SIZE, fileToSend)) > 0) {
+            fileBuffer.insert(fileBuffer.end(), readFileBuffer.data(), readFileBuffer.data() + fileReadAmount);
         }
 
-        delete[] fileBuffer;
+        if (std::regex_search(request.requestURI, htmlRegex)) {
+            snprintf(rawHTTPResponseHeader.data(), rawHTTPResponseHeader.capacity(),
+                     "%s 200 OK\nContent-Type: text/html\nContent-Length: %lu\n\n",
+                     request.httpVersion.c_str(), fileBuffer.size());
+            write(socket, rawHTTPResponseHeader.data(), strlen(rawHTTPResponseHeader.data()));
+            write(socket, fileBuffer.data(), fileBuffer.size());
+        }
+        else if (std::regex_search(request.requestURI, faviconRegex)) {
+            snprintf(rawHTTPResponseHeader.data(), rawHTTPResponseHeader.capacity(),
+                     "%s 200 OK\nContent-Type: image/x-icon\nContent-Length: %lu\n\n",
+                     request.httpVersion.c_str(), fileBuffer.size());
+            write(socket, rawHTTPResponseHeader.data(), strlen(rawHTTPResponseHeader.data()));
+            write(socket, fileBuffer.data(), fileBuffer.size());
+        }
     }
     close(socket);
-    delete[] rawResponse;
 }
 
 std::vector<std::string> HTTPRequestHandler::Split(std::string str, std::string delim) {
